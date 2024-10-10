d1464a9cfea8416f32e0f53676f8787dcbe41063
Ernur Torekul <ernurtorekul@gmail.com>
2024-10-10 08:38:57 +0500

    modified the weekdays into a map

--- app/page.tsx
+++ app/page.tsx
@@ -1,69 +1,91 @@
-import { Badge } from '@/components/ui/badge'
-import ListComponent from '@/components/custom/list_component'
-import DayComponent from '@/components/custom/day_component'
+'use client'
+import { Badge } from "@/components/ui/badge";
+import ListComponent from "@/components/custom/list_component";
+import DayComponent from "@/components/custom/day_component";
+import { useEffect, useState } from "react";
+
+const getWeekday = (date: Date) => {
+  const weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return weekdays[date.getDay()];
+};
 
 export default function Home() {
+  const [days, setDays] = useState<
+    { day: number; weekday: string; active: boolean }[]
+  >([]);
+
+  useEffect(() => {
+    const generatedDays = [];
+    const today = new Date();
+
+    for (let i = 0; i < 14; i++) {
+      const nextDate = new Date();
+      nextDate.setDate(today.getDate() + i);
+
+      generatedDays.push({
+        day: nextDate.getDate(),
+        weekday: getWeekday(nextDate),
+        active: i === 0,
+      });
+    }
+    setDays(generatedDays);
+  }, []);
+
   return (
-    <div className='p-4'>
-      <div className='my-4'>
-        <h2 className='text-lg font-bold'>Hey buddy! How are you?</h2>
+    <div className="p-4">
+      <div className="my-4">
+        <h2 className="text-lg font-bold">Hey buddy! How are you?</h2>
         {/* //TODO: overflow  */}
-        <div className='flex gap-2'>
-          <DayComponent day={12} weekday='wed' active={true} />
-          <DayComponent day={13} weekday='thu' />
-          <DayComponent day={14} weekday='fri' />
-          <DayComponent day={15} weekday='sat' />
-          <DayComponent day={16} weekday='sun' />
-          <DayComponent day={17} weekday='mon' />
-          <DayComponent day={18} weekday='tue' />
-          <DayComponent day={19} weekday='wed' />
-          <DayComponent day={20} weekday='thu' />
-          <DayComponent day={21} weekday='fri' />
-          <DayComponent day={22} weekday='sat' />
-          <DayComponent day={23} weekday='sun' />
-          <DayComponent day={24} weekday='mon' />
-          <DayComponent day={25} weekday='tue' />
+        <div className="flex gap-2 ">
+          {days.map((dayObj, index) => (
+            <DayComponent
+              key={index}
+              day={dayObj.day}
+              weekday={dayObj.weekday}
+              active={dayObj.active}
+            />
+          ))}
         </div>
       </div>
 
-      <div className='my-4'>
-        <h2 className='text-lg font-bold'>Genres</h2>
-        <div className='flex gap-2 my-2'>
+      <div className="my-4">
+        <h2 className="text-lg font-bold">Genres</h2>
+        <div className="flex gap-2 my-2">
           <Badge>All</Badge>
-          <Badge variant='outline'>Action</Badge>
-          <Badge variant='outline'>Adventure</Badge>
-          <Badge variant='outline'>Strategy</Badge>
+          <Badge variant="outline">Action</Badge>
+          <Badge variant="outline">Adventure</Badge>
+          <Badge variant="outline">Strategy</Badge>
         </div>
       </div>
 
-      <div className='my-4'>
-        <h2 className='text-lg font-bold'>Series</h2>
+      <div className="my-4">
+        <h2 className="text-lg font-bold">Series</h2>
         <ListComponent
-          title='Rick and Morty: The Anime'
+          title="Rick and Morty: The Anime"
           released={true}
-          image='https://data-vykhoda.ru/wp-content/uploads/2024/08/15.jpg'
+          image="https://data-vykhoda.ru/wp-content/uploads/2024/08/15.jpg"
         />
         <ListComponent
-          title='Rick and Morty: The Anime'
+          title="Rick and Morty: The Anime"
           released={false}
-          image='https://data-vykhoda.ru/wp-content/uploads/2024/08/15.jpg'
+          image="https://data-vykhoda.ru/wp-content/uploads/2024/08/15.jpg"
         />
         <ListComponent
-          title='Rick and Morty: The Anime'
+          title="Rick and Morty: The Anime"
           released={true}
-          image='https://data-vykhoda.ru/wp-content/uploads/2024/08/15.jpg'
+          image="https://data-vykhoda.ru/wp-content/uploads/2024/08/15.jpg"
         />
         <ListComponent
-          title='Rick and Morty: The Anime'
+          title="Rick and Morty: The Anime"
           released={true}
-          image='https://data-vykhoda.ru/wp-content/uploads/2024/08/15.jpg'
+          image="https://data-vykhoda.ru/wp-content/uploads/2024/08/15.jpg"
         />
         <ListComponent
-          title='Rick and Morty: The Anime'
+          title="Rick and Morty: The Anime"
           released={true}
-          image='https://data-vykhoda.ru/wp-content/uploads/2024/08/15.jpg'
+          image="https://data-vykhoda.ru/wp-content/uploads/2024/08/15.jpg"
         />
       </div>
     </div>
-  )
+  );
 }
